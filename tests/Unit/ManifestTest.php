052af29e40e0425a4f086d387a4679410e77dccd
Nicolas Hedger <nicolas@hedger.ch>
2022-01-12 21:01:54 +0100

    Improve static analysis

--- tests/Unit/ManifestTest.php
+++ tests/Unit/ManifestTest.php
@@ -2,36 +2,45 @@
 
 namespace Laravite\Manifest\Test\Unit;
 
+use Exception;
 use Laravite\Manifest\Chunk;
 use Laravite\Manifest\Manifest;
 
 class ManifestTest extends TestCase
 {
+    /**
+     * @throws Exception
+     */
+    private function getTestManifest(): Manifest
+    {
+        return match ($json = file_get_contents(self::FIXTURES_DIR . 'manifest.json')) {
+            false => throw new Exception('Cannot read test manifest file.'),
+            default => Manifest::parse($json),
+        };
+    }
+
     public function test_it_parses_a_json_manifest(): void
     {
-        $this->assertInstanceOf(Manifest::class, Manifest::parse(''));
+        $this->assertInstanceOf(Manifest::class, $this->getTestManifest());
     }
 
     public function test_it_returns_all_chunks(): void
     {
-        /** @phpstan-ignore-next-line */
-        $manifest = Manifest::parse(file_get_contents(self::FIXTURES_DIR . 'manifest.json'));
+        $manifest = $this->getTestManifest();
         $this->assertIsArray($manifest->chunks);
         $this->assertCount(3, $manifest->chunks);
     }
 
     public function test_it_returns_all_entries(): void
     {
-        /** @phpstan-ignore-next-line */
-        $manifest = Manifest::parse(file_get_contents(self::FIXTURES_DIR . 'manifest.json'));
+        $manifest = $this->getTestManifest();
         $this->assertIsArray($manifest->entries);
         $this->assertCount(1, $manifest->entries);
     }
 
     public function test_it_retrieves_a_specific_entry(): void
     {
-        /** @phpstan-ignore-next-line */
-        $manifest = Manifest::parse(file_get_contents(self::FIXTURES_DIR . 'manifest.json'));
+        $manifest = $this->getTestManifest();
         $entry = $manifest->entry('main.js');
         $this->assertInstanceOf(Chunk::class, $entry);
         $this->assertEquals('assets/main.4889e940.js', $entry->file);
@@ -39,9 +48,8 @@ class ManifestTest extends TestCase
 
     public function test_it_handles_not_existing_entries(): void
     {
-        /** @phpstan-ignore-next-line */
-        $manifest = Manifest::parse(file_get_contents(self::FIXTURES_DIR . 'manifest.json'));
-        $this->expectException(\Exception::class);
+        $manifest = $this->getTestManifest();
+        $this->expectException(Exception::class);
         $manifest->entry('does-not-exist.js');
     }
 }
